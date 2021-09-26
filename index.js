2e2944b08eab1b754e9981b8f72508fedde25a92
Guilherme Andrade e Chaves <83043492+guiaech@users.noreply.github.com>
2021-09-26 11:54:34 -0300

    Upload

---
+++ index.js
@@ -0,0 +1,69 @@
+const AWS = require("aws-sdk");
+
+const dynamo = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = async (event, context) => {
+  let body;
+  let statusCode = 200;
+  const headers = {
+    "Content-Type": "application/json"
+  };
+
+  try {
+    switch (event.routeKey) {
+      case "DELETE /items/{id}":
+        await dynamo
+          .delete({
+            TableName: "corebiz",
+            Key: {
+              id: event.pathParameters.id
+            }
+          })
+          .promise();
+        body = `Deleted item ${event.pathParameters.id}`;
+        break;
+      case "GET /items/{id}":
+        body = await dynamo
+          .get({
+            TableName: "corebiz",
+            Key: {
+              id: event.pathParameters.id
+            }
+          })
+          .promise();
+        break;
+      case "GET /items":
+        body = await dynamo.scan({ TableName: "corebiz" }).promise();
+        break;
+      case "PUT /items":
+        let requestJSON = JSON.parse(event.body);
+        await dynamo
+          .put({
+            TableName: "corebiz",
+            Item: {
+              id: requestJSON.id,
+              title: requestJSON.title,
+              url: requestJSON.url,
+              miniurl: requestJSON.miniurl,
+              address: requestJSON.address
+            }
+          })
+          .promise();
+        body = `Put item ${requestJSON.id}`;
+        break;
+      default:
+        throw new Error(`Unsupported route: "${event.routeKey}"`);
+    }
+  } catch (err) {
+    statusCode = 400;
+    body = err.message;
+  } finally {
+    body = JSON.stringify(body);
+  }
+
+  return {
+    statusCode,
+    body,
+    headers
+  };
+};
